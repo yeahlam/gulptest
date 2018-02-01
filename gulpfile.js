b33cd645eec7c4d752e0b00f3978e24f214acaa5
yeahlam <tedy@live.cn>
2018-02-01 11:34:07 +0800

    add concat funtion

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var less        = require('gulp-less');
 var reload      = browserSync.reload;
+const concat =require('gulp-concat')
 gulp.task('less',function(){
     gulp.src('./src/*.less')
     .pipe(less())
+    .pipe()
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('concat',function(){
+    gulp.src('./src/**/*.js')
+    .pipe(concat('main.js'))
     .pipe(gulp.dest('./dist/'))
 
 })
---
+++ src/a.js
@@ -0,0 +1,2 @@
+const log=1;
+const log=1;
